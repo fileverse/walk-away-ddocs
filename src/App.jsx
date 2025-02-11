3514c67db567ec33ee25bf35f799f8dbb250a077
mbj36 <mohitbajo36@gmail.com>
2025-02-11 21:01:37 +0530

    chore: first blood

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Walk away ddocs</h1>
+      <div className="card">Coming Soon</div>
+    </div>
+  );
+}
+
+export default App;
